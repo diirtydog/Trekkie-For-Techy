d68c2dbc5e7661599d40d0654c19a45534c9a640
diirtydog <andrew.derek.wales@gmail.com>
2022-02-20 18:07:17 -0600

    heroku is having an issue

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(session(sess));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then (() => {
+sequelize.sync({ force: true }).then (() => {
     app.listen(PORT, () => console.log('Now listening Cat Daddy!'));
 });
+
+// https://awtrekkie.herokuapp.com/
