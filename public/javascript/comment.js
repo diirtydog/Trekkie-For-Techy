fd483ff447fad058601b2f293da8231adc31dcda
diirtydog <andrew.derek.wales@gmail.com>
2022-02-24 11:30:05 -0600

    made the login routes function a little smoother and don't know why heroku was messed up

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -20,7 +20,7 @@ async function commentFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.reload();
+            document.location.reload('/single-blog');
         } else {
             alert(response.statusText);
         }
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session(sess));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then (() => {
+sequelize.sync({ force: false }).then (() => {
     app.listen(PORT, () => console.log('Now listening Cat Daddy!'));
 });
 
